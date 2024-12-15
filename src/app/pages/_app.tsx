50e59a25caff230c19a282e061d86815f68aafae
phanuel tsopze <146930921+alibia-phanuel@users.noreply.github.com>
2024-12-15 11:26:46 +0100

    add pixel facebook new

--- src/app/pages/_app.tsx
+++ src/app/pages/_app.tsx
@@ -2,9 +2,11 @@
 // pages/_app.tsx
 import { useEffect } from "react";
 import { useRouter } from "next/router";
+import { FB_PIXEL_ID, pageview } from "../../lib/fbpixel";
 import Script from "next/script";
 import * as gtag from "../../lib/gtag";
 import * as fbpixel from "../../lib/fbpixel";
+import Image from "next/image";
 
 const isProduction = process.env.NODE_ENV === "production";
 
@@ -33,6 +35,34 @@ export default function App({ Component, pageProps }: any) {
       {/* Injecter Google Analytics */}
       {isProduction && (
         <>
+          {/* Ajouter le script Pixel Facebook */}
+          <Script
+            id="fb-pixel-script"
+            strategy="afterInteractive"
+            dangerouslySetInnerHTML={{
+              __html: `
+            !function(f,b,e,v,n,t,s)
+            {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+            n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+            if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+            n.queue=[];t=b.createElement(e);t.async=!0;
+            t.src=v;s=b.getElementsByTagName(e)[0];
+            s.parentNode.insertBefore(t,s)}(window, document, 'script',
+            'https://connect.facebook.net/en_US/fbevents.js');
+            fbq('init', '${FB_PIXEL_ID}');
+            fbq('track', 'PageView');
+          `,
+            }}
+          />
+          <noscript>
+            <Image
+              height="1"
+              width="1"
+              style={{ display: "none" }}
+              src={`https://www.facebook.com/tr?id=${FB_PIXEL_ID}&ev=PageView&noscript=1`}
+              alt={""}
+            />
+          </noscript>
           <Script
             id="fb-pixel"
             strategy="afterInteractive"
--- src/lib/fbpixel.ts
+++ src/lib/fbpixel.ts
@@ -1,13 +1,19 @@
-// lib/fbpixel.ts
-
 export const FB_PIXEL_ID = "1543728419655118"; // Remplacez par votre Pixel ID
 
 // Enregistrer une page vue
 export const pageview = () => {
-  window.fbq("track", "PageView");
+  if (typeof window !== "undefined" && typeof window.fbq === "function") {
+    window.fbq("track", "PageView");
+  } else {
+    console.warn("Pixel Facebook non détecté");
+  }
 };
 
 // Enregistrer un événement personnalisé
 export const event = (name: string, options: Record<string, any> = {}) => {
-  window.fbq("track", name, options);
+  if (typeof window !== "undefined" && typeof window.fbq === "function") {
+    window.fbq("track", name, options);
+  } else {
+    console.warn("Pixel Facebook non détecté");
+  }
 };
